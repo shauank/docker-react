e62d3deffb9adf1056d52574b4f8be193b3c50f7
Shaunak Patel <shaunak@Shaunaks-MacBook-Pro.local>
2022-06-11 19:39:16 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          There!!
+          There!! SOme 
         </p>
         <a
           className="App-link"
